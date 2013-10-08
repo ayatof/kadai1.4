e25d4493d262ccaced503c82fd9db6a9674a30e6
ayatof <fujita-ayato@in.kayac.com>
2013-10-08 12:30:56 +0900

    Update ugokanai.js

---
+++ public/javascripts/ugokanai.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	var appendHTML = "";
+    $.getJSON("http://127.0.0.1:3000/home_timeline.json", function(data){
+	//$(".top").append("<h1 class='title'>"+data.+"</h1>");
+        $.each(data, function(i,item){
+            var txt = item.text
+			    .replace(/\"/,'&quot;')
+                .replace(/(https?:\/\/[-a-z0-9._~:\/?#@!$&\'()*+,;=%]+)/ig,'<a href="$1">$1</a>')
+                .replace(/@+([_A-Za-z0-9-]+)/ig, '<a href="https://twitter.com/$1">@$1</a>')
+                .replace(/#+([_A-Za-z0-9-]+)/ig, '<a href="https://twitter.com/search?q=$1">#$1</a>');	
+			appendHTML += "<article class='tweet'>\n"+
+				"<header class='user_profile'>\n"+
+					"<a class='user_name' href='https://twitter.com/"+
+					item.user.screen_name+"'>@"+item.user.screen_name+"&nbsp;/&nbsp;"+item.user.name+
+					"</a>\n"+
+				"</header>\n"+
+			    "<p class='text'>"+txt+"</p>\n"+
+			"</article>\n"; 
+			
+			/*$(".sec_tweets").append("<article class='tweet'>\n"+
+				"<header class='user_profile'>\n"+
+					"<a class='user_name' href='https://twitter.com/"+
+					item.user.screen_name+"'>@"+item.user.screen_name+"&nbsp;/&nbsp;"+item.user.name+
+					"</a>\n"+
+				"</header>\n"+
+			    "<p class='text'>"+txt+"</p>\n"+
+			"</article>\n");*/
+			
+        });
+    });
+	$(".sec_tweets").append(appendHTML);
+});
+
